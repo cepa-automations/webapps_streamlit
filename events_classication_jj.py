98845a3e2d332a553209073773a49f7ed29ce2aa
vhossaki <vhossaki@cepasafedrive.com>
2024-02-09 13:29:37 -0300

    First commit

---
+++ events_classication_jj.py
@@ -0,0 +1,15 @@
+# app.py
+import streamlit as st
+
+def main():
+    st.title("My Streamlit App")
+    st.write("This is a basic Streamlit app.")
+
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
+
+if __name__ == "__main__":
+    main()
